c4e3c95af03ea0ad09c6c672666f1ffa96801ba9
Lucas Martins <lucascamargogoncalvesmartins@gmail.com>
2025-08-21 20:25:44 -0300

    aaa

--- src/View/CadastraAdm.php
+++ src/View/CadastraAdm.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <title>Cadastro de Usuário</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+ 
 </head>
 <body>
 
