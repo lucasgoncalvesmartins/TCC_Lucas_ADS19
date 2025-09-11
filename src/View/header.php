80dcb6b26c1e14766bb2273b48f534e87f430786
Lucas Martins <lucascamargogoncalvesmartins@gmail.com>
2025-09-11 01:45:43 -0300

    Adcionado Tab Index

--- src/View/header.php
+++ src/View/header.php
@@ -20,20 +20,20 @@ if (session_status() === PHP_SESSION_NONE) {
         <nav aria-label="Menu principal">
             <div class="nav-container">
                 <?php if (!isset($_SESSION['id']) || (isset($_SESSION['tipo']) && $_SESSION['tipo'] === 'comum')): ?>
-                    <a href="Home.php">Home</a>
-                    <a href="UsuarioSugestao.php">Enviar Sugestões</a>
-                    <a href="#">Contato</a>
-                    <a href="#">Sobre</a>
-                    <a href="AdmLogin.php">Login</a>
+                    <a href="Home.php"  tabindex="0">Home</a>
+                    <a href="UsuarioSugestao.php"  tabindex="0">Enviar Sugestões</a>
+                    <a href="#"  tabindex="0">Contato</a>
+                    <a href="#"  tabindex="0">Sobre</a>
+                    <a href="AdmLogin.php"  tabindex="0">Login</a>
                 <?php endif; ?>
 
                 <?php if (isset($_SESSION['id']) && ($_SESSION['tipo'] === 'autor' || $_SESSION['tipo'] === 'admin')): ?>
-                    <a href="SecaoCadastrar.php">Adicionar Seção</a>
-                    <a href="SecaoEditar.php">Editar Seção</a>
-                    <a href="SubSecaoCadastrar.php">Adicionar SubSeção</a>
-                    <a href="SubSecaoEditar-Excluir.php">Editar SubSeção</a>
+                    <a href="SecaoCadastrar.php"  tabindex="0">Adicionar Seção</a>
+                    <a href="SecaoEditar.php"  tabindex="0">Editar Seção</a>
+                    <a href="SubSecaoCadastrar.php"  tabindex="0">Adicionar SubSeção</a>
+                    <a href="SubSecaoEditar-Excluir.php"  tabindex="0">Editar SubSeção</a>
                     <?php if ($_SESSION['tipo'] === 'admin'): ?>
-                        <a href="UsuarioListar.php">Usuários</a>
+                        <a href="UsuarioListar.php"  tabindex="0">Usuários</a>
                     <?php endif; ?>
                 <?php endif; ?>
 
